627c54caa2ac15c4f37da36858d209e0d293a074
Palash26200 <palash@webkorps.com>
2022-08-17 01:39:32 +0530

    alpha blog

---
+++ README.md
@@ -0,0 +1,4 @@
+* rails g migrate create_articles
+* rails db:migrate
+* rails db:rollback
+* rails routes --expanded
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def show
+        # @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        # @article = Article.find(params[:id])
+    end
+
+    def update
+        # @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to articles_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def create
+        # flash[:notice] = "ok"
+        # render plain: params[:article]
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+            flash[:notice] = "Article was created successfully."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        # @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private 
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+end
